1b3992bdc1e3a26f1245ecbd8faaa9b4048bc185
Matt Hosch <matt_hosch@hotmail.com>
2020-12-01 16:38:50 -0600

    CRUD operational

---
+++ src/actions/index.js
@@ -0,0 +1,56 @@
+import streams from "../apis/streams";
+import history from "../history";
+import {
+  SIGN_IN,
+  SIGN_OUT,
+  CREATE_STREAM,
+  DELETE_STREAM,
+  EDIT_STREAM,
+  FETCH_STREAM,
+  FETCH_STREAMS,
+} from "./types";
+
+export const signIn = (userId) => {
+  return {
+    type: SIGN_IN,
+    payload: userId,
+  };
+};
+
+export const signOut = () => {
+  return {
+    type: SIGN_OUT,
+  };
+};
+
+export const createStream = (formValues) => async (dispatch, getState) => {
+  const { userId } = getState().auth;
+  const { data } = await streams.post("/streams", { ...formValues, userId });
+  dispatch({ type: CREATE_STREAM, payload: data });
+  // After stream creation, navigate user to root route
+  history.push("/");
+};
+
+export const fetchStreams = () => async (dispatch) => {
+  const { data } = await streams.get("/streams");
+  dispatch({ type: FETCH_STREAMS, payload: data });
+};
+
+export const fetchStream = (id) => async (dispatch) => {
+  const { data } = await streams.get(`/streams/${id}`);
+  dispatch({ type: FETCH_STREAM, payload: data });
+};
+
+export const editStream = (id, formValues) => async (dispatch) => {
+  const { data } = await streams.patch(`/streams/${id}`, formValues);
+  dispatch({ type: EDIT_STREAM, payload: data });
+  // After stream edit, navigate user to root route
+  history.push("/");
+};
+
+export const deleteStream = (id) => async (dispatch) => {
+  await streams.delete(`/streams/${id}`);
+  dispatch({ type: DELETE_STREAM, payload: id });
+  // After stream deletion, navigate user to root route
+  history.push("/");
+};
